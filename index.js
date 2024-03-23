1c51b94ee8e8f7e9d14f0a1ec11d1a45e7db9b90
hy2n <me@donghyun.cc>
2024-03-23 11:11:53 +0900

    add api request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const axios = require('axios');
+const request = require('request');
 
 // 8000번 포트로 지정
 const port = 8000;
@@ -12,11 +13,12 @@ app.set("view engine", "ejs");
 // Express에서 정적파일 제공
 app.use('/static', express.static('static'));
 
+
 // test.ejs 실행
 app.get("/", (req, res) => {
-    let today = new Date(); 
-    res.render("food",{
-        name:'Current Time : ' + today
+    let today = new Date();
+    res.render("food", {
+        name: 'Current Time : ' + today
     });
 })
 
@@ -24,3 +26,26 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log("SERVER started : ", port);
 })
+
+app.get("/request", (req, res) => {
+    var nx = '';
+    var ny = '';
+    const options = {
+        uri: "https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=G10&SD_SCHUL_CODE=7430310&MLSV_YMD=202403&type=json",
+        qs: {
+            gridx: nx,
+            gridy: ny
+        }
+    };
+    request(options, function (err, response, body) {
+        if (err) {
+            // 오류가 발생하면 클라이언트에게 오류 메시지를 응답으로 보냅니다.
+            return res.status(500).send("서버 오류 발생");
+        }
+        var obj = JSON.parse(body);
+        console.log(obj);
+
+        // 요청에 대한 응답을 클라이언트에게 보냅니다.
+        res.json(obj);
+    });
+});
